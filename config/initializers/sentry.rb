b6634d4a24331479fa590358f614f5bb266fcc12
emilweth <coucou@emi.cool>
2024-03-17 18:23:59 +0100

    feat: add sentry env name

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,15 +1,12 @@
 if ENV['SENTRY_DSN'].present?
   Sentry.init do |config|
     config.dsn = ENV['SENTRY_DSN']
-    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    config.traces_sample_rate = 0.1
-    # or
-    config.traces_sampler = lambda do |context|
-      true
+    if ENV['SENTRY_ENVIRONMENT'].present?
+      config.environment = ENV['SENTRY_ENVIRONMENT']
     end
+
+    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+    config.traces_sample_rate = 0.1
   end
 end
